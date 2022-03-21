0501acfa88ead197d3fb5131d2eaa80e0a6ca6c0
Davyd Kewen <dcsistemas3@gmail.com>
2022-03-21 12:29:31 -0300

    refactor: correction url link

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 const bool applicationDevelopment = true;
-const String urlDev = "http://7971-168-195-132-41.ngrok.io/api";
+const String urlDev = "http://ba97-168-195-132-41.ngrok.io/api";
 const String urlProd = "http://localhost:3000/api";
 
 final maskPhoneNumber = MaskTextInputFormatter(
